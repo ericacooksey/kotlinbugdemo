eac06a26f800c967394980a66f5251f41a579c07
Erica Cooksey <erica.cooksey@gmail.com>
2018-03-20 18:28:38 -0700

    First commit

---
+++ app/src/main/java/example/erica/kotlinbugdemo/StarRatingView.kt
@@ -0,0 +1,82 @@
+package example.erica.kotlinbugdemo
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.util.Log
+import android.view.Gravity
+import android.widget.ImageView
+import android.widget.LinearLayout
+
+class StarRatingView : LinearLayout {
+    private val TAG = StarRatingView::class.java.simpleName
+    private var imgStarOpen: Drawable
+    private var imgStarFilled: Drawable
+    private var numStars: Int
+    private var rating: Int
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        // Set up the layout
+        this.orientation = LinearLayout.HORIZONTAL
+        this.gravity = Gravity.CENTER_HORIZONTAL
+        // Obtain the star drawables
+        imgStarOpen = context.getDrawable(R.drawable.ic_star_open)
+        imgStarFilled = context.getDrawable(R.drawable.ic_star_filled)
+        // Set the attributes styled in XML
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.StarRatingView)
+        numStars = typedArray.getInt(R.styleable.StarRatingView_numStars, 5)
+        rating = typedArray.getInt(R.styleable.StarRatingView_rating, 0)
+        typedArray.recycle()
+
+        // Set up the view
+        val dimen = context.resources.getDimensionPixelSize(R.dimen.star_rating_dimen)
+        val margin = context.resources.getDimensionPixelSize(R.dimen.star_rating_margin)
+        var i: Int = 0
+        // Iterate over the number of stars, adding each one to the layout
+        while (i < numStars) {
+            val starImageView: ImageView = ImageView(context, null, R.style.StarRating_Star)
+            // Use filled stars if rating is initialized
+            if (i < rating) {
+                starImageView.background = imgStarFilled
+            } else {
+                starImageView.background = imgStarOpen
+            }
+            // Set a click listener so that tapping the star sets it as the rating
+            starImageView.setOnClickListener {
+                setRating(i + 1) // index + 1 is the rating
+            }
+            // Set the margin in the layout params
+            var layoutParams: LinearLayout.LayoutParams = LinearLayout.LayoutParams(dimen, dimen)
+            if (i < numStars - 1) {
+                layoutParams.rightMargin = margin
+            }
+            // Add the star as a child view
+            addView(starImageView, layoutParams)
+            i++
+        }
+    }
+
+    /**
+     * Exposed method to set the rating
+     */
+    fun setRating(newRating: Int) {
+        this.rating = newRating
+        var i: Int = 0
+        while (i < numStars) {
+            var imageView: ImageView = this.getChildAt(i) as ImageView
+            if (i < rating) {
+                imageView.background = imgStarFilled
+            } else {
+                imageView.background = imgStarOpen
+            }
+            i++
+        }
+    }
+
+    /**
+     * @return The user-defined rating
+     */
+    fun getRating(): Int {
+        return this.rating
+    }
+}
